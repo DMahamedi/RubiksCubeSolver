aa41ccc444e816aff8fe6162ced9d091adb9ec4f
Darius Mahamedi <dmaha004@ucr.edu>
2023-10-19 16:13:30 -0700

    Added ThistlewaiteCube class

---
+++ ThistlewaiteCube.py
@@ -0,0 +1,17 @@
+from Cube import Cube
+
+class Thistlewaite(Cube):
+    
+    def __init__(self, white=['w']*9, red=['r']*9, blue=['b']*9, yellow=['y']*9, green=['g']*9, orange=['o']*9):
+        super().__init__(white, red, blue, yellow, green, orange)
+
+    #G1 functions
+    
+
+    #G2 functions
+
+
+    #G3 functions
+
+
+    #G4 functions
